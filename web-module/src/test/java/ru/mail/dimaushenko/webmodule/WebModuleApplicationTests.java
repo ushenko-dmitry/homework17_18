7f9026b974372b6c3ecb1dabd26c6140b1df6a7a
Unknown <dimaushenko@mail.ru>
2020-03-04 03:11:12 +0300

    init web proj, add properties and resources

---
+++ web-module/src/test/java/ru/mail/dimaushenko/webmodule/WebModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mail.dimaushenko.webmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
